689df87d4a28c97cedf962ac6fa228e8de7045d6
Miles Allain <allain@mymail.mines.edu>
2020-09-24 10:41:42 -0600

    added exception handling to ScoreTrakker

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -6,20 +6,28 @@ import java.util.Scanner;
 
 public class ScoreTrakker {
 	private ArrayList<Student> students;
-	
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
 	public ScoreTrakker() {
 		students = new ArrayList<Student>();
 	}
 	
 	public void loadDataFromFile(String inputFile) throws FileNotFoundException {
+		students = new ArrayList<Student>();
 		FileReader reader = new FileReader(inputFile);
 		Scanner in = new Scanner(reader);
 		while(in.hasNextLine()) {
 			String name = in.nextLine();
-			int score = in.nextInt();
+			String scoreString = in.nextLine();
+			int score;
+			try{
+				score = Integer.parseInt(scoreString);
+			}
+			catch(Exception NumberFormatExecption) {
+				System.out.println("\nIncorrect format for " +  name + "not a valid score: " + scoreString + "\n");		
+				continue;
+			}
 			students.add(new Student(name,score));
-			if(!in.hasNextLine()) {break;}
-			in.nextLine();
+			
 		}
 		in.close();
 	}
@@ -33,13 +41,18 @@ public class ScoreTrakker {
 		}
 	}
 	public void processFiles() {
-		try {
-			loadDataFromFile("scores.txt");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for (String i : files) {
+			try {
+				loadDataFromFile(i);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				
+				System.out.println("\nCan't open file: " + i);
+				continue;
+			}
+			printInOrder();
 		}
-		printInOrder();
+		
 	}
 
 	public static void main(String args[]) {
